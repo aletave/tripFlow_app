01dad61cc22b84204186aef80184ccda260d49a9
fhf7yq27mk-coder <francescoedome@icloud.com>
2026-04-27 16:28:12 +0200

    Booking e Review services aggiunti e collegati al pom parent

---
+++ booking-service/src/test/java/com/tripflow/booking/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripflow.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

d255b6968fe511873e7798bc3f313d3fc9a7a926
mattia747 <mattiataccone47@gmail.com>
2026-04-29 17:15:11 +0200

    Update config

--- itinerary-service/src/main/java/com/tripflow/itinerary_service/model/Itinerary.java
+++ itinerary-service/src/main/java/com/tripflow/itinerary_service/model/Itinerary.java
@@ -1,4 +1,70 @@
 package com.tripflow.itinerary_service.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "itineraries")
 public class Itinerary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "owner_id", nullable = false)
+    private Long ownerId;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, length = 30)
+    private String visibility;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    // Relazione con itinerary-stops
+    @OneToMany(
+            mappedBy = "itinerary",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<ItineraryStop> stops = new ArrayList<>();
+
+    // Costruttore vuoto
+    public Itinerary() {
+    }
+
+    // viene eseguito prima di salvare per la prima volta un oggetto nel DB
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    // viene eseguito prima di aggiornare un oggetto nel DB
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
--- itinerary-service/src/main/java/com/tripflow/itinerary_service/model/ItineraryStop.java
+++ itinerary-service/src/main/java/com/tripflow/itinerary_service/model/ItineraryStop.java
@@ -1,4 +1,58 @@
 package com.tripflow.itinerary_service.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+//si occuperà lombok per fare getter e setter in maniere automatico
+@Getter
+@Setter
+@Entity
+@Table(name = "itinerary_stops")
 public class ItineraryStop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "stop_order", nullable = false)
+    private Integer stopOrder;
+
+    @Column(name = "stop_type", nullable = false, length = 30)
+    private String stopType;
+
+    @Column(name = "viaggio_id")
+    private Long viaggioId;
+
+    @Column(name = "attivita_id")
+    private Long attivitaId;
+
+    @Column(name = "custom_title", length = 150)
+    private String customTitle;
+
+    @Column(name = "custom_description", columnDefinition = "TEXT")
+    private String customDescription;
+
+    @Column(name = "start_datetime")
+    private LocalDateTime startDatetime;
+
+    @Column(name = "end_datetime")
+    private LocalDateTime endDatetime;
+
+    @Column(columnDefinition = "TEXT")
+    private String notes;
+
+    //costruttore vuoto
+    public ItineraryStop() {
+    }
+
+    //gestisce la relazione tra itinerary stop (1) -> itineary(n)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "itinerary_id", nullable = false)
+    private Itinerary itinerary;
+
+
+
 }

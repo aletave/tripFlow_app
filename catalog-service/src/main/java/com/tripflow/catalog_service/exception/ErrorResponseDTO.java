41b419b12c44377ded0cce664725229938a1075d
umbyyy <umbertofrancescomessina@gmail.com>
2026-05-02 18:04:19 +0200

    aggiunta classi exception (dto resource n.f. e global exc. handler) e fix pom catalog service

---
+++ catalog-service/src/main/java/com/tripflow/catalog_service/exception/ErrorResponseDTO.java
@@ -0,0 +1,17 @@
+package com.tripflow.catalog_service.exception;
+
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponseDTO {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+}

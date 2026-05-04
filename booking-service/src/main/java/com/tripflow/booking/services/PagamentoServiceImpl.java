c5fe8de960cb43f9528170ee2caa36b0d708afdb
francescomolinaroo <fhf7yq27mk@privaterelay.appleid.com>
2026-05-04 21:36:41 +0200

    Creazione delle interfacce Service

---
+++ booking-service/src/main/java/com/tripflow/booking/services/PagamentoServiceImpl.java
@@ -0,0 +1,6 @@
+package com.tripflow.booking.services;
+
+import com.tripflow.booking.data.service.PagamentoService;
+
+public class PagamentoServiceImpl implements PagamentoService {
+}
---
+++ booking-service/src/main/java/com/tripflow/booking/services/PrenotazioneServiceImpl.java
@@ -0,0 +1,6 @@
+package com.tripflow.booking.services;
+
+import com.tripflow.booking.data.service.PrenotazioneService;
+
+public class PrenotazioneServiceImpl implements PrenotazioneService {
+}

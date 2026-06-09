16a8d583aefd3f390e35bb6272188229488f807a
francescomolinaroo <fhf7yq27mk@privaterelay.appleid.com>
2026-06-09 14:55:26 +0200

    Sistemato bug DiscoveryService, creato file application.yml con struttura e inserite rotte per booking-service e review-service

--- discovery-service/src/main/java/com/tripflow/discovery/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/tripflow/discovery/discoveryservice/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.tripflow.discoveryservice;
+package com.tripflow.discovery.discoveryservice;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- gateway-service/src/main/java/com/tripflow/gateway/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/tripflow/gateway/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,18 @@ package com.tripflow.gateway.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+//TODO implementare DNSResolutionFixer nella fase di dockerizzazione
+// Main del modulo gateway-service.
+// @EnableDiscoveryClient: il gateway si registra su Eureka come gli altri micro
+// e usa il registry per risolvere gli uri "lb://<service-name>" delle route.
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(GatewayServiceApplication.class, args);
+        new SpringApplicationBuilder(GatewayServiceApplication.class).run(args);
     }
-
 }

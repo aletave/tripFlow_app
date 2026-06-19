0fa0b3d06fb01682ff69e64566fe7fa7ffe5002a
mattia747 <mattiataccone47@gmail.com>
2026-06-19 16:03:32 +0200

    Modifica discoveryService x le connessioni con gli altri microservizi

--- gateway-service/src/test/test/com/tripflow/gateway/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/test/com/tripflow/gateway/gatewayservice/GatewayServiceApplicationTests.java
@@ -3,11 +3,9 @@ package com.tripflow.gateway.gatewayservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.tripflow.gateway.GatewayServiceApplication.class)
 class GatewayServiceApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }

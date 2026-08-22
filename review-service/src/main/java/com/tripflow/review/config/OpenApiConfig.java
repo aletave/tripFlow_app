48df4d29cd3a217207913b85a1804e4090f97cf5
mattia747 <mattiataccone47@gmail.com>
2026-08-22 16:36:12 +0200

    add springdoc dependencies and OpenAPI configuration

---
+++ review-service/src/main/java/com/tripflow/review/config/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.tripflow.review.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//Configurazione OpenAPI: abilita il pulsante "Authorize" nella Swagger UI
+//dichiarando lo schema bearer (JWT Keycloak) usato dagli endpoint protetti.
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+
+                .info(new Info()
+                        .title("Review Service API")
+                        .version("1.0.0")
+                        .description("Documentazione delle api rest delle recensioni di TripFlow"))
+
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
